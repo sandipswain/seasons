6d84e6a43369c4a648675e5463e4194850de2d60
Sandip Swain <swainsandip9@gmail.com>
2022-01-20 08:15:31 +0530

    Done with section 6 of course

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 // function App() {
 //   window.navigator.geolocation.getCurrentPosition(
 //     (position) => console.log(position),
@@ -8,32 +10,42 @@ import React from "react";
 // }
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    // This is the only time when we do direct assignment to this.state
-    this.state = {
-      lat: null,
-      errorMessage: "",
-    };
+  // constructor(props) {
+  //   super(props);
+  //   // This is the only time when we do direct assignment to this.state
+  //   this.state = {
+  //     lat: null,
+  //     errorMessage: "",
+  //   };
+  // }
+
+  state = { lat: null, errorMessage: "" };
+  componentDidMount() {
+    // This method is called when the component gets rendered for the first time
+    console.log("My Component was rendered to the screen");
+    // To update our state object we call setState
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({
-          // To update our state object we call setState
-          lat: position.coords.latitude,
-        });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
+
+  componentDidUpdate() {
+    console.log("My component was updated - it rerendered!");
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat)
+      return <div>Error: {this.state.errorMessage}</div>;
+    if (!this.state.errorMessage && this.state.lat)
+      // return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
+    return <Spinner message="Please accept the location request" />;
+  }
+
   // React says we have to define render()
   render() {
-    if (this.state.errorMessage && !this.state.lat)
-      return <div>Error: {this.state.errorMessages}</div>;
-    if (!this.state.errorMessage && this.state.lat)
-      return <div>Latitude: {this.state.lat}</div>;
-    return <div>Loading...</div>;
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,30 @@
+import "./SeasonDisplay.css";
 import React from "react";
 
-function SeasonDisplay() {
-  return <div>Season Display</div>;
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Burr it is cold!",
+    iconName: "snowflake",
+  },
+};
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) return lat > 0 ? "summer" : "winter";
+  else return lat > 0 ? "winter" : "summer";
+};
+function SeasonDisplay(props) {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+  return (
+    <div className={`seasonDisplay ${season}`}>
+      <i className={`iconLeft massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`iconRight massive ${iconName} icon`} />
+    </div>
+  );
 }
 
 export default SeasonDisplay;
