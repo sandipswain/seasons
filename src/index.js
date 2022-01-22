749a238d3b4bb9fa4d9ebc231042116e16156a85
Sandip Swain <swainsandip9@gmail.com>
2022-01-22 22:42:11 +0530

    Did upto Context solving changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import "./index.css";
-import App from "./App";
+// import App from "./Seasons/App";
+import App from "./Pics/App";
 
 ReactDOM.render(
   <React.StrictMode>
